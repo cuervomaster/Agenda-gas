33b2e80049a0d749e84797756ae5934751181b9c
pruebaGAS <maestrocuervo11@gmail.com>
2024-08-27 09:58:02 -0500

    Avance en el video 9 del curso v1

--- funciones.js
+++ funciones.js
@@ -7,3 +7,11 @@ function obtenerDatosHTML(nombre)
 {
     return HtmlService.createHtmlOutputFromFile(nombre).getContent() ;
 }
+
+function obtenerContactos()
+{
+    let hoja = SpreadsheetApp.openById('1b6ne84pOH7YZ1m-Iu4WkYGjxssgFGCgnJN2oiNyxHdc').getActiveSheet();
+    let datos = hoja.getDataRange().getValues();
+    return datos;
+
+}
